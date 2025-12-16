93a2aa5bf588a76e0630d2dc0536ed4bafd49331
alexew0923 <70456996+alexew0923@users.noreply.github.com>
2025-12-16 15:04:44 -0400

    Update doGet.js

--- Codes/Apps_Script/doGet.js
+++ Codes/Apps_Script/doGet.js
@@ -1,4 +1,4 @@
-const sheet_id = "1iJzvixnEx5QH2lkQNkN8xKZqpIyGO7FmEsa_qsyHCOI";
+const sheet_id = "1iJzvixnEx5QH2lkQNkN8xKZqpIyGO7FmEsa_qsyHCOI"; //get the Google Sheets and individual sheets
 const ss = SpreadsheetApp.openById(sheet_id);
 const sheet = ss.getSheetByName("weather_station");
 const archiveSheet = ss.getSheetByName("ArchivedAverages");
@@ -6,8 +6,8 @@ const numberSheet = ss.getSheetByName("Number_of_Data_Received");
 const historicalSheet = ss.getSheetByName("HistoricalData");
 const graphSheet = ss.getSheetByName("Graphs");
 
-function doGet(e){
-  const temp = String(e.parameter.temp);
+function doGet(e){ //executed when the receiver sends the URL
+  const temp = String(e.parameter.temp); //get individual data from the URL
   const hum = String(e.parameter.hum);
   const soil = String(e.parameter.soil);
   const air = String(e.parameter.air);
@@ -17,17 +17,15 @@ function doGet(e){
   const time = new Date();
 
   const values = [time, temp, hum, soil, air, rain, snow, count];
-  for (let i = 1; i < 8; i++) {
-    if (isNaN(values[i])) {
-      values[i] = "";
+  for (let i = 1; i < 7; i++) {
+    if (isNaN(values[i]) || i > 1 && values[i] == 0) { //check for NaN values and 0 for every other data except the temperature
+      values[i] = ""; //if that is the case, set the value to empty
     }
   }
 
-  const lastRow = sheet.getLastRow();
-  const lastValue = sheet.getRange(lastRow, 8).getValue();
   Logger.log(values);
 
-  if (count != lastValue && count != 0) {
+  if (count != 0) {
     sheet.appendRow(values);
     const graphTitle = ["Temperature (°C)", "Air Pressure (hPa)", "Snow Depth (cm)"]
     const graphRange = ["B2:B", "E2:E", "G2:G"]
