3237de796f89ff387a0d4dab16c5175458780be0
alexew0923 <70456996+alexew0923@users.noreply.github.com>
2026-04-01 03:31:37 -0300

    Update transmitter.ino

--- scripts/weather_station/transmitter/transmitter.ino
+++ scripts/weather_station/transmitter/transmitter.ino
@@ -3,8 +3,7 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <Adafruit_BMP280.h>
-#include <DHT.h>
-#include <DHT_U.h>
+#include "Adafruit_SHT4x.h"
 #include <Wire.h>
 
 #define uS_TO_S_FACTOR 1000000ULL  // Conversion factor for micro seconds to seconds
@@ -12,16 +11,14 @@
 RTC_DATA_ATTR int bootCount = 0;
 
 #define SOIL_SENSOR_PIN 36
-#define trigPin 25 //Ultrasonic sensor to measure the snow depth
-#define echoPin 33
-#define SOIL_SNOW_POWER_PIN 32
+#define SOIL_POWER_PIN 32
 
 #define RAIN_SENSOR_PIN 39
 #define RAIN_POWER_PIN 26
 
-#define DHTPIN 16
-#define DHTTYPE DHT22
-DHT dht(DHTPIN, DHTTYPE);
+Adafruit_SHT4x sht4 = Adafruit_SHT4x();
+
+#define BATTERY_PIN 33
 
 struct SensorPacket { //New structure to store data
   float temperature;        // DHT22 temperature
@@ -29,19 +26,27 @@ struct SensorPacket { //New structure to store data
   int soil;                 // Soil moisture %
   float pressure;           // BMP280 pressure in kPa
   int rain;                 // Rain sensor value
-  int snow;               // Snow depth
+  float battery;              //Measures battery voltage
   unsigned long bootCount;  // Number of times device booted
 };
 
 RF24 radio(4, 5); // CE, CSN
 const byte address[6] = "80923"; //Same address for the receiver
 
-Adafruit_BMP280 bmp; // I2C
+TwoWire I2COne = TwoWire(0);
+TwoWire I2CTwo = TwoWire(1);
+
+Adafruit_BMP280 bmp(&I2COne); //I2C
 
 void setup() {
   delay(500);
   //Serial.begin(115200); //uncomment serial print lines when debugging
-  dht.begin();
+
+  I2COne.begin(21, 22);
+  I2CTwo.begin(2, 16);
+  sht4.begin(&I2CTwo);
+  sht4.setPrecision(SHT4X_HIGH_PRECISION);
+  sht4.setHeater(SHT4X_NO_HEATER);
   bmp.begin(0x76);
   bmp.setSampling(
     Adafruit_BMP280::MODE_FORCED,
@@ -51,18 +56,20 @@ void setup() {
   );
   pinMode(RAIN_POWER_PIN, OUTPUT); //Setting pins for sensors
   digitalWrite(RAIN_POWER_PIN, LOW);
-  pinMode(SOIL_SNOW_POWER_PIN, OUTPUT);
-  digitalWrite(SOIL_SNOW_POWER_PIN, LOW);
-  pinMode(trigPin, OUTPUT);
-  digitalWrite(trigPin, LOW);
-  pinMode(echoPin, INPUT);
+  pinMode(SOIL_POWER_PIN, OUTPUT);
+  digitalWrite(SOIL_POWER_PIN, LOW);
+
   radio.begin(); //Initializing NRF24L01
-  radio.openWritingPipe(address);
+  radio.setChannel(76);
+  radio.setDataRate(RF24_250KBPS);
   radio.setPALevel(RF24_PA_MAX);
-  radio.stopListening();
+  radio.setRetries(2, 3);
+  radio.powerUp();
+  radio.stopListening(address);
   ++bootCount;
   //Serial.println("Boot number: " + String(bootCount));
   sensor();
+  radio.powerDown();
   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
   esp_deep_sleep_start();
 }
@@ -70,54 +77,43 @@ void setup() {
 void sensor() {
   SensorPacket data;
 
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  sensors_event_t humidity, temp;
+  sht4.getEvent(&humidity, &temp); //populate temp and humidity objects with fresh data
+  data.humidity = humidity.relative_humidity;
+  data.temperature = temp.temperature;
 
   bmp.takeForcedMeasurement();
-  float hectopascal = bmp.readPressure()/100.0;
+  data.pressure = bmp.readPressure()/100.0;
 
   digitalWrite(RAIN_POWER_PIN, HIGH);
   delay(100);
-  int rainValue = analogRead(RAIN_SENSOR_PIN);
+  data.rain = analogRead(RAIN_SENSOR_PIN);
   digitalWrite(RAIN_POWER_PIN, LOW);
   
-  long distance = 0;
-  int moist_percent = 0;
-  digitalWrite(SOIL_SNOW_POWER_PIN, HIGH);
+  digitalWrite(SOIL_POWER_PIN, HIGH);
   delay(200);
-  if (t < 5) { //Measure snow depth when temperature is below five degrees zero and soil moisture is it is above zero
-    if (bootCount%6 == 0) { //Measure every 30 minutes
-      digitalWrite(trigPin, LOW);
-      delayMicroseconds(5);
-      digitalWrite(trigPin, HIGH);
-      delayMicroseconds(100);
-      digitalWrite(trigPin, LOW);
-      long duration = pulseIn(echoPin, HIGH);
-      float speed = (331.4+0.606*t)/10000;
-      distance = duration * speed / 2;
-    }
+  if (bootCount%6 == 0 && data.temperature > 0) { //Measure every 30 minutes
+    data.soil = analogRead(SOIL_SENSOR_PIN);
+  } else {
+    data.soil = 0;
   }
+  digitalWrite(SOIL_POWER_PIN, LOW);
 
-  if (t > 0) {
-    moist_percent = analogRead(SOIL_SENSOR_PIN);
+  for (int i = 0; i < 100; i++) {
+    data.battery += analogReadMilliVolts(BATTERY_PIN);
   }
-  digitalWrite(SOIL_SNOW_POWER_PIN, LOW);
+  data.battery = data.battery/100.0;
+  data.battery = 98.6/98.4 * data.battery + data.battery - 30; //In millivolts
 
-  data.temperature = t;
-  data.humidity = h;
-  data.soil = moist_percent;
-  data.pressure = hectopascal;
-  data.rain = rainValue;
-  data.snow = distance;
   data.bootCount = bootCount;
 
-  /*Serial.print("Temp: "); Serial.print(t);
-  Serial.print(", Hum: "); Serial.print(h);
-  Serial.print(", Soil: "); Serial.print(moist_percent);
-  Serial.print(", Pressure: "); Serial.print(hectopascal);
-  Serial.print(", Rain: "); Serial.print(rainValue);
-  Serial.print(", Snow: "); Serial.print(distance);
-  Serial.print(", Boot: "); Serial.println(bootCount);
+  /*Serial.print("Temp: "); Serial.print(data.temperature);
+  Serial.print(", Hum: "); Serial.print(data.humidity);
+  Serial.print(", Soil: "); Serial.print(data.soil);
+  Serial.print(", Pressure: "); Serial.print(data.pressure);
+  Serial.print(", Rain: "); Serial.print(data.rain);
+  Serial.print(", Battery Voltage: "); Serial.print(data.battery);
+  Serial.print(", Boot: "); Serial.println(data.bootCount);
   Serial.flush();*/
 
   // Send the struct
