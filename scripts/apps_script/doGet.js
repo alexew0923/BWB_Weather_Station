7e8687db41a827dcfb860b93a8c2797a0a876315
alexew0923 <70456996+alexew0923@users.noreply.github.com>
2026-04-05 02:02:24 -0300

    Update doGet.js

--- scripts/apps_script/doGet.js
+++ scripts/apps_script/doGet.js
@@ -12,22 +12,22 @@ function doGet(e){ //executed when the receiver sends the URL
   const soil = String(e.parameter.soil);
   const air = String(e.parameter.air);
   const rain = String(e.parameter.rain);
-  const snow = String(e.parameter.snow);
+  const battery = String(e.parameter.bat);
   const count = String(e.parameter.count);
   const time = new Date();
 
-  const values = [time, temp, hum, soil, air, rain, snow, count];
+  const values = [[time, temp, hum, soil, air, rain, battery, count]];
   for (let i = 1; i < 7; i++) {
-    if (isNaN(values[i]) || i > 1 && values[i] == 0) { //check for NaN values and 0 for every other data except the temperature
-      values[i] = ""; //if that is the case, set the value to empty
+    if (isNaN(values[0][i]) || values[0][i] == 0) { //check for NaN values and 0 for every other data except the temperature
+      values[0][i] = ""; //if that is the case, set the value to empty
     }
   }
 
   Logger.log(values);
 
   if (count != 0) {
-    sheet.appendRow(values);
-    const graphTitle = ["Temperature (°C)", "Air Pressure (hPa)", "Snow Depth (cm)"]
+    sheet.getRange(sheet.getLastRow() + 1, 1, 1, 8).setValues(values);
+    const graphTitle = ["Temperature (°C)", "Air Pressure (hPa)", "Battery Voltage (mV)"]
     const graphRange = ["B2:B", "E2:E", "G2:G"]
     changeYAxis(graphTitle, graphRange, "weather_station");
   }
