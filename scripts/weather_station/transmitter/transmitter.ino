055acffad66dcebf384d012701162fc79090d5e4
alexew0923 <70456996+alexew0923@users.noreply.github.com>
2026-06-19 11:42:03 -0300

    Update transmitter.ino

--- scripts/weather_station/transmitter/transmitter.ino
+++ scripts/weather_station/transmitter/transmitter.ino
@@ -1,26 +1,23 @@
-//Set CPU frequency to 80MHz to save power
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
-#include <Adafruit_BMP280.h>
-#include "Adafruit_SHT4x.h"
-#include <Wire.h>
+#include <Adafruit_BMP280.h> //library for BMP280 air pressure sensor
+#include "Adafruit_SHT4x.h" //library for SHT40 temperature & humidity sensor
+#include <Wire.h> //library for I2C communication
+#include <esp_now.h> //library for espnow
+#include <WiFi.h> //library for WiFi which is needed to run espnow
 
-#define uS_TO_S_FACTOR 1000000ULL  // Conversion factor for micro seconds to seconds
-#define TIME_TO_SLEEP 299          //Sleep time, currently at 5 minutes, accounting for the time it takes to run sensor()
+#define uS_TO_S_FACTOR 1000000ULL  //conversion factor for micro seconds to seconds
+#define TIME_TO_SLEEP 300          //sleep time in seconds, currently set at 5 minutes
 RTC_DATA_ATTR int bootCount = 0;
 
-#define SOIL_SENSOR_PIN 36
-#define SOIL_POWER_PIN 32
-
-#define RAIN_SENSOR_PIN 39
-#define RAIN_POWER_PIN 26
-
-#define TRANSISTOR_PIN 17
+#define SOIL_SENSOR_PIN 4
+#define RAIN_SENSOR_PIN 3
+#define TRANSISTOR_PIN 5
+#define BATTERY_PIN 2
 
 Adafruit_SHT4x sht4 = Adafruit_SHT4x();
+Adafruit_BMP280 bmp;
 
-#define BATTERY_PIN 33
+uint8_t broadcastAddress[] = {0xe8, 0xf6, 0x0a, 0x15, 0x19, 0xfc};
+esp_now_peer_info_t peerInfo;
 
 struct SensorPacket {       //New structure to store data
   float temperature;        // DHT22 temperature
@@ -31,22 +28,18 @@ struct SensorPacket {       //New structure to store data
   float battery;            //Measures battery voltage
   unsigned long bootCount;  // Number of times device booted
 };
+SensorPacket data;
 
-RF24 radio(4, 5);                 // CE, CSN
-const byte address[6] = "80923";  //Same address for the receiver
-
-TwoWire I2COne = TwoWire(0);
-TwoWire I2CTwo = TwoWire(1);
-
-Adafruit_BMP280 bmp(&I2COne);  //I2C
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  //Serial.print("\r\nLast Packet Send Status:\t");
+  //Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
 
 void setup() {
-  delay(500);
+  delay(500) //prevent infinte deep sleep bug
   //Serial.begin(115200);  //uncomment serial print lines when debugging
-
-  I2COne.begin(21, 22);
-  I2CTwo.begin(2, 16);
-  sht4.begin(&I2CTwo);
+  Wire.begin(6, 7);
+  sht4.begin(&Wire);
   sht4.setPrecision(SHT4X_HIGH_PRECISION);
   sht4.setHeater(SHT4X_NO_HEATER);
   bmp.begin(0x76);
@@ -55,33 +48,41 @@ void setup() {
     Adafruit_BMP280::SAMPLING_X2,
     Adafruit_BMP280::SAMPLING_X4,
     Adafruit_BMP280::FILTER_X4);
-  pinMode(RAIN_POWER_PIN, OUTPUT);  //Setting pins for sensors
-  digitalWrite(RAIN_POWER_PIN, LOW);
-  pinMode(SOIL_POWER_PIN, OUTPUT);
-  digitalWrite(SOIL_POWER_PIN, LOW);
   pinMode(TRANSISTOR_PIN, OUTPUT);
   digitalWrite(TRANSISTOR_PIN, HIGH);
-  delay(500);
+  
+  WiFi.mode(WIFI_STA);
 
-  radio.begin();  //Initializing NRF24L01
-  radio.setChannel(76);
-  radio.setDataRate(RF24_250KBPS);
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setRetries(2, 3);
-  radio.powerUp();
-  radio.stopListening(address);
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    //Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(esp_now_send_cb_t(OnDataSent));
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    //Serial.println("Failed to add peer");
+    return;
+  }
+
   ++bootCount;
   //Serial.println("Boot number: " + String(bootCount));
   sensor();
-  radio.powerDown();
-  digitalWrite(TRANSISTOR_PIN, LOW);
   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  delay(10);
   esp_deep_sleep_start();
 }
 
 void sensor() {
-  SensorPacket data;
-
   sensors_event_t humidity, temp;
   sht4.getEvent(&humidity, &temp);  //populate temp and humidity objects with fresh data
   data.humidity = humidity.relative_humidity;
@@ -89,28 +90,29 @@ void sensor() {
 
   bmp.takeForcedMeasurement();
   data.pressure = bmp.readPressure() / 100.0;
-
-  digitalWrite(RAIN_POWER_PIN, HIGH);
-  delay(100);
+  
+  digitalWrite(TRANSISTOR_PIN, LOW);
+  delay(10);
   data.rain = analogRead(RAIN_SENSOR_PIN);
-  digitalWrite(RAIN_POWER_PIN, LOW);
+  data.soil = analogRead(SOIL_SENSOR_PIN);
+  digitalWrite(TRANSISTOR_PIN, HIGH);
 
-  digitalWrite(SOIL_POWER_PIN, HIGH);
-  delay(200);
-  if (bootCount % 6 == 0 && data.temperature > 0) {  //Measure every 30 minutes
-    data.soil = analogRead(SOIL_SENSOR_PIN);
-  } else {
-    data.soil = 0;
-  }
-  digitalWrite(SOIL_POWER_PIN, LOW);
-
-  for (int i = 0; i < 100; i++) {
+  for (int i = 0; i < 100; i++) { //read battery voltage 100 times to average it out
     data.battery += analogReadMilliVolts(BATTERY_PIN);
   }
   data.battery = data.battery / 100.0;
-  data.battery = 98.6 / 98.4 * data.battery + data.battery;  //In millivolts
+  data.battery = 98.6 / 98.4 * data.battery + data.battery + 35;  //battery voltage in millivolts with 35mV offset
 
   data.bootCount = bootCount;
+
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &data, sizeof(data));
+   
+  if (result == ESP_OK) {
+    //Serial.println("Sent with success");
+  }
+  else {
+    //Serial.println("Error sending the data");
+  }
 
   /*Serial.print("Temp: ");
   Serial.print(data.temperature);
@@ -127,9 +129,6 @@ void sensor() {
   Serial.print(", Boot: ");
   Serial.println(data.bootCount);
   Serial.flush();*/
-
-  // Send the struct
-  radio.write(&data, sizeof(data));
 }
 
 void loop() {
