4420f55369bcb82e460fc13555a3805c77b4be3a
alexew0923 <70456996+alexew0923@users.noreply.github.com>
2026-09-02 13:59:48 +0900

    Update and rename updateGraphAxis.js to Upddate_Graph.js

--- scripts/apps_script/Upddate_Graph.js
+++ scripts/apps_script/Upddate_Graph.js
@@ -1,18 +1,21 @@
 function changeYAxis(graphTitle, graphRange, sheet) {
+  const charts = graphSheet.getCharts();
   const dataSheet = ss.getSheetByName(sheet);
-  let charts = graphSheet.getCharts();
-  charts.forEach(function(chart){
+
+  charts.forEach(function(chart) {
     for (let i = 0; i < graphTitle.length; i++) {
-      if(chart.getOptions().get("title") === graphTitle[i]) {
+      if(chart.getOptions().get("title") == graphTitle[i]) {
         // Define the range containing the data you want to use for min/max calculation
         const dataRange = dataSheet.getRange(graphRange[i]); 
         const dataValues = dataRange.getValues();
-        // Flatten the array and remove empty cells to find min and max
-        const flatValues = dataValues.map(row => row[0]).filter(String);
+
+        // Convert 2D array to 1D array
+        const flatValues = dataValues.flat().filter(isNumber);
+        Logger.log(flatValues);
         let minVal = Math.min(...flatValues);
         let maxVal = Math.max(...flatValues);
-
-        // Optional: Add a buffer to min/max for better visualization
+  
+        // Add a buffer to min/max for better visualization
         const buffer = (maxVal - minVal) * 0.1; // 10% buffer
         minVal = minVal - buffer;
         maxVal = maxVal + buffer;
@@ -25,12 +28,16 @@ function changeYAxis(graphTitle, graphRange, sheet) {
 
         // Modify the chart's y-axis
         chart = chart.modify()
-          .setOption('vAxes.0.viewWindow.min', minVal)
-          .setOption('vAxes.0.viewWindow.max', maxVal)
+          .setOption('vAxis.viewWindow.min', minVal)
+          .setOption('vAxis.viewWindow.max', maxVal)
           .build();
-
         graphSheet.updateChart(chart);
+        break;
       }
     }
   });
+}
+
+function isNumber(value) {
+  return typeof value === "number";
 }
